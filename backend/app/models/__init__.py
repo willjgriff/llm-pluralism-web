d3c3595fbcd457711f2e61bfdb6aeadb9c11b8ad
willjgriff <willjgriff@gmail.com>
2026-04-01 14:24:34 +0100

    Delete everything

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Model package exports."""
-
-from app.models.questionnaire_response import QuestionnaireResponse
-from app.models.rating import Rating
-from app.models.session import Session
-
-__all__ = ["Session", "Rating", "QuestionnaireResponse"]
